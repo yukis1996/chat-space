db7ae4dda32fb377f32218baa614aa9671632174
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2019-12-18 09:27:14 +0900

    インクリメンタルサーチ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    respond_to do |format|
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"]).where.not(id: current_user.id).limit(10)
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
 
