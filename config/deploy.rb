cb87b8c7b30649cefe094615499eeda4ec8dd0cf
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2019-12-23 15:16:55 +0900

    awss3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+set :linked_files, %w{ config/secrets.yml }
+
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
@@ -31,4 +33,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
